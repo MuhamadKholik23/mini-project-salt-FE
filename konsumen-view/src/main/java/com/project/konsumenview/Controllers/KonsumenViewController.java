ba9e3c5c494fc93b58bca21a8ed6a364dd0d08dc
muhamadkholik23 <muhamadkholikduatilu@gmail.com>
2023-11-07 08:50:35 +0700

    add filter

--- konsumen-view/src/main/java/com/project/konsumenview/Controllers/KonsumenViewController.java
+++ konsumen-view/src/main/java/com/project/konsumenview/Controllers/KonsumenViewController.java
@@ -5,8 +5,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.project.konsumenview.models.Konsumen;
+import com.project.konsumenview.models.KonsumenFilter;
 
 
 @Controller
@@ -15,10 +17,14 @@ public class KonsumenViewController {
   private final String backendUrl = "http://localhost:8080/konsumen";
 
     @GetMapping("/home")
-    public String konsumenPage(Model model) {
-        RestTemplate restTemplate = new RestTemplate();
-        Konsumen[] listKonsumen = restTemplate.getForObject(backendUrl + "/getAll", Konsumen[].class);
-        model.addAttribute("listKonsumen", listKonsumen);
-        return "home";
+    public String konsumenPage(Model model, KonsumenFilter f) {
+      RestTemplate restTemplate = new RestTemplate();
+      UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(backendUrl + "/getAll")
+        .queryParam("nama", f.getNama())
+        .queryParam("kota", f.getKota());
+
+      Konsumen[] listKonsumen = restTemplate.getForObject(builder.toUriString(), Konsumen[].class);
+      model.addAttribute("listKonsumen", listKonsumen);
+      return "home";
     }
 }
---
+++ konsumen-view/src/main/java/com/project/konsumenview/models/KonsumenFilter.java
@@ -0,0 +1,9 @@
+package com.project.konsumenview.models;
+
+import lombok.Data;
+
+@Data
+public class KonsumenFilter {
+  private String nama;
+  private String kota;
+}
